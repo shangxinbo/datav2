64ce6c928955cde2a657f34604bdda211bcd4712
shangxinbo <314911714@qq.com>
2017-05-17 11:47:29 +0800

    首屏

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,42 +1,42 @@
 <template>
     <div class="system-warp">
         <div class="system">
-            <img class="bg" src="img/good-chart.png" />
+            <img class="bg" :src="bg" />
             <div class="system-content">
                 <h2>集奥聚合产品体系</h2>
-                <img class="planet-line" src="img/planet-line01.png" />
+                <img class="planet-line" :src="planetLine1" />
                 <div class="system-planet-warp">
-                    <iframe class="planet-iframe" allowtransparency="true" src="planet/planet-middle.html"></iframe>
+                    <iframe class="planet-iframe" allowtransparency="true" src="/static/planet/planet-middle.html"></iframe>
                     <div class="satellite">
                         <span></span>
                     </div>
                     <div class="system-icon">
-                        <img src="img/system-icon01.png" />
+                        <img :src="systemIcon1" />
                     </div>
                 </div>
                 <div class="planet-left">
-                    <iframe class="planet-iframe" allowtransparency="true" src="planet/planet-left.html"></iframe>
+                    <iframe class="planet-iframe" allowtransparency="true" src="/static/planet/planet-left.html"></iframe>
                     <h3>营销产品</h3>
                     <div class="system-icon">
-                        <img src="img/system-icon02.png" />
+                        <img :src="systemIcon2" />
                     </div>
                 </div>
                 <div class="planet-right">
-                    <iframe class="planet-iframe" allowtransparency="true" src="planet/planet-right.html"></iframe>
+                    <iframe class="planet-iframe" allowtransparency="true" src="/static/planet/planet-right.html"></iframe>
                     <h3>风控产品</h3>
                     <div class="system-icon">
-                        <img src="img/system-icon03.png" />
+                        <img :src="systemIcon3" />
                     </div>
                 </div>
                 <div class="system-left">
-                    <img class="line" src="img/planet-line02.png" />
+                    <img class="line" :src="planetLine2" />
                     <div class="small-planet-warp">
                         <strong>画像描述</strong>
                         <div class="small-planet">
                             <span></span>
                         </div>
                         <div class="system-icon">
-                            <img src="img/system-icon04.png" />
+                            <img :src="systemIcon4" />
                         </div>
                     </div>
                     <div class="small-planet-warp">
@@ -45,7 +45,7 @@
                             <span></span>
                         </div>
                         <div class="system-icon">
-                            <img src="img/system-icon05.png" />
+                            <img :src="systemIcon5" />
                         </div>
                     </div>
                     <div class="small-planet-warp">
@@ -54,7 +54,7 @@
                             <span></span>
                         </div>
                         <div class="system-icon">
-                            <img style="margin: -3px -4px 0 0;" src="img/system-icon06.png" />
+                            <img style="margin: -3px -4px 0 0;" :src="systemIcon6" />
                         </div>
                     </div>
                     <div class="small-planet-warp">
@@ -63,19 +63,19 @@
                             <span></span>
                         </div>
                         <div class="system-icon">
-                            <img style="margin: 0 -4px -6px 0;" src="img/system-icon07.png" />
+                            <img style="margin: 0 -4px -6px 0;" :src="systemIcon7" />
                         </div>
                     </div>
                 </div>
                 <div class="system-right">
-                    <img class="line" src="img/planet-line03.png" />
+                    <img class="line" :src="planetLine3" />
                     <div class="small-planet-warp">
                         <strong>反欺诈</strong>
                         <div class="small-planet">
                             <span></span>
                         </div>
                         <div class="system-icon">
-                            <img src="img/system-icon08.png" />
+                            <img :src="systemIcon8" />
                         </div>
                     </div>
                     <div class="small-planet-warp">
@@ -84,7 +84,7 @@
                             <span></span>
                         </div>
                         <div class="system-icon">
-                            <img style="margin: 0 -5px -4px 0;" src="img/system-icon09.png" />
+                            <img style="margin: 0 -5px -4px 0;" :src="systemIcon9" />
                         </div>
                     </div>
                     <div class="small-planet-warp">
@@ -93,7 +93,7 @@
                             <span></span>
                         </div>
                         <div class="system-icon">
-                            <img src="img/system-icon10.png" />
+                            <img :src="systemIcon10" />
                         </div>
                     </div>
                     <div class="small-planet-warp">
@@ -102,7 +102,7 @@
                             <span></span>
                         </div>
                         <div class="system-icon">
-                            <img style="margin: 0 0 0 -5px;" src="img/system-icon11.png" />
+                            <img style="margin: 0 0 0 -5px;" :src="systemIcon11" />
                         </div>
                     </div>
                     <div class="small-planet-warp">
@@ -111,7 +111,7 @@
                             <span></span>
                         </div>
                         <div class="system-icon">
-                            <img src="img/system-icon12.png" />
+                            <img :src="systemIcon12" />
                         </div>
                     </div>
                 </div>
@@ -152,11 +152,41 @@
     </div>
 </template>
 <script>
-    import welcome from 'assets/img/welcome.png'
+    import bg from 'assets/img/good-chart.png'
+    import planetLine1 from 'assets/img/planet-line01.png'
+    import planetLine2 from 'assets/img/planet-line02.png'
+    import planetLine3 from 'assets/img/planet-line03.png'
+    import systemIcon1 from 'assets/img/system-icon01.png'
+    import systemIcon2 from 'assets/img/system-icon02.png'
+    import systemIcon3 from 'assets/img/system-icon03.png'
+    import systemIcon4 from 'assets/img/system-icon04.png'
+    import systemIcon5 from 'assets/img/system-icon05.png'
+    import systemIcon6 from 'assets/img/system-icon06.png'
+    import systemIcon7 from 'assets/img/system-icon07.png'
+    import systemIcon8 from 'assets/img/system-icon08.png'
+    import systemIcon9 from 'assets/img/system-icon09.png'
+    import systemIcon10 from 'assets/img/system-icon10.png'
+    import systemIcon11 from 'assets/img/system-icon11.png'
+    import systemIcon12 from 'assets/img/system-icon12.png'
     export default {
         data() {
             return {
-                welcome
+                bg,
+                planetLine1,
+                planetLine2,
+                planetLine3,
+                systemIcon1,
+                systemIcon2,
+                systemIcon3,
+                systemIcon4,
+                systemIcon5,
+                systemIcon6,
+                systemIcon7,
+                systemIcon8,
+                systemIcon9,
+                systemIcon10,
+                systemIcon11,
+                systemIcon12,
             }
         }
     }
